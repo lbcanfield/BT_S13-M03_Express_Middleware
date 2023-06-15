aac6a6f945b3810eacbb0214a2d078b22818ab45
lbcanfield <lewie.b.canfield@gmail.com>
2023-06-15 10:29:00 -0500

    Initial Commit - node packages installed - /src/index.js -- base server defined - /src/api/server.js -- express server can now parse JSON data

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const server = require('./api/server');
+
+const PORT = 1701;
+
+server.listen(PORT, () => {
+     console.log(`Server Connected:  Listening on http://localhost:${PORT}`)
+})
